37b9305199c5408848ec95ddbfd583209a400423
Maximilian Tabet <maxtabet@gmail.com>
2020-11-27 18:35:50 -0500

    commit tree 0dbe78cd2202cae794cafe1c56facc57a5ce6265

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
-# Your code goes here!
+class Anagram
+    
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(new_word)
+        new_word.select do |word|
+            word.split("").sort == @word.split("").sort
+        end
+    end
+end
